6b8390b635c67fb3ab552b2271da496a0eabba9e
Gerwald <gerwald@live.de>
2022-06-20 17:21:26 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// for basic CRUD api
+// public routes, without authentication
+Route::resource('products', ProductController::class);
+
+// we need to implement this for authentication
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::post('/products', [ProductController::class, 'store']);
+//Route::get('/products/{product}', [ProductController::class, 'show']);
+//Route::put('/products/{product}', [ProductController::class, 'update']);
+//Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+// protected route
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+});
+// public route
+//Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
